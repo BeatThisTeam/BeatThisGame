07a57b731411782013bd09886ce23c7c3fe9e490
Giacomo <jack.vannu@gmail.com>
2018-10-31 15:16:20 +0100

    GroundSections stores the transform of the faces of all the rings

---
+++ BeatThisGame/Assets/Scripts/GroundSections.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ProBuilder.Addons;
+using ProBuilder.Core;
+using ProBuilder.MeshOperations;
+
+public class GroundSections : MonoBehaviour {
+
+    [System.Serializable]
+    public class Ring {
+
+        public List<Transform> faces;
+    }
+
+    public List<Ring> rings = new List<Ring>();
+}
