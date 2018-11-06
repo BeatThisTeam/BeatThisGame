5d7fb10ab16f566a593bcda490b15aeedbee839f
Martina (AstroSocks) <teslapancake@outlook.it>
2018-11-06 16:53:43 +0100

    start working on slice attack

--- BeatThisGame/Assets/Scripts/PrototypeScene/ScenePrototypeManager.cs
+++ BeatThisGame/Assets/Scripts/PrototypeScene/ScenePrototypeManager.cs
@@ -28,6 +28,7 @@ public class ScenePrototypeManager : MonoBehaviour {
 
     public Attack1 att1;
     public Attack2 att2;
+    public SliceAttack att3;
 
     private void Awake() {
         if (instance != null && instance != this) {
@@ -48,6 +49,7 @@ public class ScenePrototypeManager : MonoBehaviour {
             
             att1.StartAttack(noteToPlayInSeconds, notesInSecondsIndex);
             att2.StartAttack(noteToPlayInSeconds);
+            att3.StartAttack(noteToPlayInSeconds);
         }
     }
 
