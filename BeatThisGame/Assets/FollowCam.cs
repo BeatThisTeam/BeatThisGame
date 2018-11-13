cd3fb51c5968696307040098899882e0288b4d1f
Piotr Kozak <>
2018-11-13 14:44:02 +0100

    Added a FollowCam script to Main Camera

---
+++ BeatThisGame/Assets/FollowCam.cs
@@ -0,0 +1,30 @@
+﻿using System.Runtime.CompilerServices;
+using UnityEditor.ShaderGraph;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+
+    public Transform target;
+    public Transform player;
+
+    // Vector3 startPos = new Vector3(0f,30f,-70f);
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown("a"))
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, 40);
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            transform.RotateAround(Vector3.zero, Vector3.up, -40);
+        }
+        transform.LookAt(target);
+
+
+    }
+
+}
