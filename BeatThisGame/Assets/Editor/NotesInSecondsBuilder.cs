eb99ac81be495bff7035d045ab2ae905c8c7eae6
Giacomo <jack.vannu@gmail.com>
2018-11-13 15:11:07 +0100

    Editor extension to generate the list notesToPlayInSeconds

---
+++ BeatThisGame/Assets/Editor/NotesInSecondsBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(ScenePrototypeManager))]
+public class NotesInSecondsBuilder : Editor {
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        ScenePrototypeManager script = (ScenePrototypeManager)target;
+        if(GUILayout.Button("Create Array of Notes in Seconds")) {
+            script.setNotesInSeconds();
+        }
+    }
+}
+
+//[CustomEditor(typeof(ObjectBuilderScript))]
+//public class ObjectBuilderEditor : Editor {
+//    public override void OnInspectorGUI() {
+//        DrawDefaultInspector();
+
+//        ObjectBuilderScript myScript = (ObjectBuilderScript)target;
+//        if (GUILayout.Button("Build Object")) {
+//            myScript.BuildObject();
+//        }
+//    }
+//}
