400d2f0bd02899e482e05e8ebc7abdf0f35dc9a3
Martina (AstroSocks) <teslapancake@outlook.it>
2018-11-19 01:37:31 +0100

    Added functions to GroundColorChanger

--- BeatThisGame/Assets/Scripts/PrototypeScene/GroundColorChanger.cs
+++ BeatThisGame/Assets/Scripts/PrototypeScene/GroundColorChanger.cs
@@ -5,7 +5,8 @@ using ProBuilder.Addons;
 using ProBuilder.Core;
 using ProBuilder.MeshOperations;
 
-public class GroundColorChanger : MonoBehaviour {
+public class GroundColorChanger : MonoBehaviour
+{
 
     public GroundSections groundSections;
 
@@ -15,10 +16,12 @@ public class GroundColorChanger : MonoBehaviour {
     public Material red;
     public Material blue;
 
-    void Update() {
+    void Update()
+    {
 
         //test
-        if (Input.GetKeyDown(KeyCode.Mouse0)) {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
 
             //ChangeColor(0);
             //ChangeColor(1);
@@ -33,22 +36,30 @@ public class GroundColorChanger : MonoBehaviour {
     /// Changes the color of an entire ring
     /// </summary>
     /// <param name="ringIndex">index of the ring, 0 is the inner one</param>
-    void ChangeColor(int ringIndex) {
+    void ChangeColor(int ringIndex)
+    {
 
-        if (ringIndex < groundSections.rings.Count) {
+        if (ringIndex < groundSections.rings.Count)
+        {
 
-            for (int i = 0; i < groundSections.rings[ringIndex].faces.Count; i++) {
+            for (int i = 0; i < groundSections.rings[ringIndex].faces.Count; i++)
+            {
                 Renderer rendererFaces;
                 rendererFaces = groundSections.rings[ringIndex].faces[i].GetComponent<Renderer>();
 
-                if (rendererFaces.sharedMaterial == blue) {
+                if (rendererFaces.sharedMaterial == blue)
+                {
                     rendererFaces.material = red;
-                } else {
+                }
+                else
+                {
                     rendererFaces.material = blue;
                 }
             }
 
-        } else {
+        }
+        else
+        {
             Debug.LogError("ERROR: ringIndex out of bound");
         }
     }
@@ -58,15 +69,18 @@ public class GroundColorChanger : MonoBehaviour {
     /// </summary>
     /// <param name="x">index of the ring</param>
     /// <param name="z">index of the face</param>
-    public void ChangeColor(int ringIndex, int faceIndex) {
+    public void ChangeColor(int ringIndex, int faceIndex)
+    {
 
-        if (ringIndex > groundSections.rings.Count) {
+        if (ringIndex > groundSections.rings.Count)
+        {
 
             Debug.LogError("ERROR: ringIndex out of bound");
             return;
         }
 
-        if (faceIndex > groundSections.rings[ringIndex].faces.Count) {
+        if (faceIndex > groundSections.rings[ringIndex].faces.Count)
+        {
 
             Debug.LogError("ERROR: faceIndex out of bound");
             return;
@@ -74,9 +88,14 @@ public class GroundColorChanger : MonoBehaviour {
 
         Renderer rendererFaces;
         rendererFaces = groundSections.rings[ringIndex].faces[faceIndex].GetComponent<Renderer>();
-        if (rendererFaces.sharedMaterial == blue) {
+        if (rendererFaces.sharedMaterial == blue)
+        {
             rendererFaces.material = red;
-        } else {
+            //Debug.Log("red " + SongManager.Instance.SongPositionInBeats + " " + SongManager.Instance.SongPositionInBars);
+        }
+        else
+        {
+          //  Debug.Log("blue " + SongManager.Instance.SongPositionInBeats + " " + SongManager.Instance.SongPositionInBars);
             rendererFaces.material = blue;
         }
     }
@@ -84,10 +103,49 @@ public class GroundColorChanger : MonoBehaviour {
 
     public void ChangeColorSlice(int faceIndex)
     {
-
         ChangeColor(0, faceIndex);
         ChangeColor(1, faceIndex);
         ChangeColor(2, faceIndex);
 
+
     }
+
+    public void Red(int ringIndex, int faceIndex)
+    {
+        Renderer rendererFaces;
+        rendererFaces = groundSections.rings[ringIndex].faces[faceIndex].GetComponent<Renderer>();
+        if (rendererFaces.sharedMaterial == blue)
+        {
+            rendererFaces.material = red;
+            
+        }
+    }
+
+    public void AllRed(int faceIndex)
+    {
+        Red(0, faceIndex);
+        Red(1, faceIndex);
+        Red(2, faceIndex);
+    }
+
+    public void AllBlue(int faceIndex)
+    {
+        Blue(0, faceIndex);
+        Blue(1, faceIndex);
+        Blue(2, faceIndex);
+    }
+
+    public void Blue(int ringIndex, int faceIndex)
+    {
+        Renderer rendererFaces;
+        rendererFaces = groundSections.rings[ringIndex].faces[faceIndex].GetComponent<Renderer>();
+        if (rendererFaces.sharedMaterial == red)
+        {
+            rendererFaces.material = blue;
+
+        }
+    }
+
+    
 }
+
