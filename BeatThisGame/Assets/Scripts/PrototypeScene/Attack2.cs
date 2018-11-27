27e6994474fc5cb038020d62361a8bf4a2a8b8b5
Giacomo <jack.vannu@gmail.com>
2018-11-27 14:39:43 +0100

    Refactoring Attack2 and added new functionality to shoot more projectiles

--- BeatThisGame/Assets/Scripts/PrototypeScene/Attack2.cs
+++ BeatThisGame/Assets/Scripts/PrototypeScene/Attack2.cs
@@ -10,26 +10,69 @@ public class Attack2 : MonoBehaviour {
 
     public Transform player;
     public Transform boss;
+    public GroundSections groundSections;
 
     public float spawnHeight = 12f;
-    //public float projectileSpeed = 20f;
     private float lastNotePlayedInSeconds;
 
-    public void StartAttack(float noteToPlayInSeconds )
-    {
+    private int initialTargetSection;
+    private int nAttacks = 0;
 
-        Vector3 spawnPos = new Vector3(boss.position.x, spawnHeight, boss.position.z);
+    private Vector3 spawnPos;
+    private CharacterController playerContr;
+
+    public void StartAttack(float noteToPlayInSeconds){
+
+        
+        playerContr = player.GetComponent<CharacterController>();
+        initialTargetSection = playerContr.faceIndex;
+        spawnPos = new Vector3(boss.position.x, spawnHeight, boss.position.z);
         Vector3 targetPos = new Vector3(player.position.x, spawnHeight, player.position.z);
         //float projectileTime = Vector3.Distance(spawnPos, new Vector3(player.position.x, spawnHeight, player.position.z)) / projectileSpeed;
-        float projectileTime = ScenePrototypeManager.Instance.notesInSeconds[ScenePrototypeManager.Instance.notesInSecondsIndex + 1].notePosInSeconds - noteToPlayInSeconds;
+        //float projectileTime = ScenePrototypeManager.Instance.notesInSeconds[ScenePrototypeManager.Instance.notesInSecondsIndex + 1].notePosInSeconds - noteToPlayInSeconds;
 
-        if (noteToPlayInSeconds - SongManager.Instance.SongPositionInSeconds <= projectileTime && noteToPlayInSeconds != 0 && lastNotePlayedInSeconds != noteToPlayInSeconds)
-        {
+        if (noteToPlayInSeconds != 0 && lastNotePlayedInSeconds != noteToPlayInSeconds){
+
+            float projectileTime = noteToPlayInSeconds - SongManager.Instance.SongPositionInSeconds;
+            if(projectileTime <= 0.1) {
+                projectileTime = ScenePrototypeManager.Instance.notesInSeconds[ScenePrototypeManager.Instance.notesInSecondsIndex + 1].notePosInSeconds - noteToPlayInSeconds;
+            }
+            //Debug.Log(SongManager.Instance.SongPositionInSeconds);
+            nAttacks++;
             lastNotePlayedInSeconds = noteToPlayInSeconds;
             Projectile pr;
             pr = Instantiate(projectile, spawnPos, Quaternion.identity);
             pr.Move(spawnPos, targetPos, projectileTime);
             //ScenePrototypeManager.Instance.IncrementNoteToPlayInSeconds();
+        }
+    }
+
+    public void ContinueAttack(float noteToPlayInSeconds) {
+
+        
+        int ringIndex = playerContr.ringIndex;
+        int numberOfSections = groundSections.rings[0].sections.Count;
+        
+        if (noteToPlayInSeconds != 0 && lastNotePlayedInSeconds != noteToPlayInSeconds) {
+            //Debug.Log(SongManager.Instance.SongPositionInSeconds);
+            float projectileTime = noteToPlayInSeconds - SongManager.Instance.SongPositionInSeconds;
+            lastNotePlayedInSeconds = noteToPlayInSeconds;
+            //if (nAttacks == 1) {
+                nAttacks++;
+                //Debug.Log("att2");
+                Projectile pr1;
+                Projectile pr2;
+
+                int targetSection = (initialTargetSection + numberOfSections + 1) % numberOfSections;
+                Vector3 targetPos = new Vector3(groundSections.rings[ringIndex].sections[targetSection].tr.position.x, spawnHeight, groundSections.rings[ringIndex].sections[targetSection].tr.position.z);
+                pr1 = Instantiate(projectile, spawnPos, Quaternion.identity);
+                pr1.Move(spawnPos, targetPos, projectileTime);
+                targetSection = (initialTargetSection + numberOfSections - 1) % numberOfSections;
+                targetPos = new Vector3(groundSections.rings[ringIndex].sections[targetSection].tr.position.x, spawnHeight, groundSections.rings[ringIndex].sections[targetSection].tr.position.z);
+                pr2 = Instantiate(projectile, spawnPos, Quaternion.identity);
+                pr2.Move(spawnPos, targetPos, projectileTime);
+
+            //}
         }
     }
 }
