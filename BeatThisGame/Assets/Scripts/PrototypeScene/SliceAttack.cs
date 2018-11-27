6e4e2d5c02b6e02ee3ef7685624e6c7fdb77b37b
Giacomo <jack.vannu@gmail.com>
2018-11-27 14:36:18 +0100

    Refactoring SliceAttack

--- BeatThisGame/Assets/Scripts/PrototypeScene/SliceAttack.cs
+++ BeatThisGame/Assets/Scripts/PrototypeScene/SliceAttack.cs
@@ -7,12 +7,12 @@ public class SliceAttack : MonoBehaviour
     public Transform _player;
     public Transform _ground;
     public Transform _wall;
-    public Transform _centralring;
+    //public Transform _centralring;
 
     public int LimitTime;
 
     int RingIndex;
-    int FaceIndex;
+    int faceIndex;
     int newFaceIndex = 0;
     int storeFaceIndex;
     int allredFaceIndex;
@@ -21,205 +21,159 @@ public class SliceAttack : MonoBehaviour
     int sx = 0;
     int dx = 0;
     int y = 0;
+    private bool set = false;
 
+    //-1 left +1 right
+    private int direction = 0;
+    private int index;
 
     public float endHeight = 7f;
     public float spawnHeight = 7f;
-    public float WallSpeed = 10f;
-    public float WaitingTime;
-
-
-    public Wall wall;
+    int sliceCount;
+    public Wall wallPrefab;
+    private Wall wall;
 
     public GroundColorChanger ground;
     public CharacterController player;
     public GroundSections groundSections;
 
+    public Material mat1;
+    public Material mat2;
 
-    public void StartAttack(float noteToPlayInSeconds)
-    {
-        int SliceCount = groundSections.rings[0].faces.Count;
+    public void StartAttack(float noteToPlayInSeconds){
+
+        sliceCount = groundSections.rings[0].sections.Count;
+
+        if (noteToPlayInSeconds - SongManager.Instance.SongPositionInSeconds <= 0){
+
+            faceIndex = player.faceIndex;
+            storeFaceIndex = player.faceIndex + sliceCount;
+            allredFaceIndex = player.faceIndex + sliceCount;
+
+            Vector3 spawnPos = new Vector3(groundSections.rings[2].sections[faceIndex].tr.position.x, spawnHeight, groundSections.rings[2].sections[faceIndex].tr.position.z);
+            Vector3 endPos = new Vector3(groundSections.rings[2].sections[faceIndex].tr.position.x, endHeight, groundSections.rings[2].sections[faceIndex].tr.position.z);
+
+            float WallTime = ScenePrototypeManager.Instance.notesInSeconds[ScenePrototypeManager.Instance.notesInSecondsIndex + 1].notePosInSeconds - noteToPlayInSeconds;
+
+            wall = Instantiate(wallPrefab, spawnPos, Quaternion.identity);
+            wall.MoveUpDown(spawnPos, endPos, WallTime);
+
+            ground.ChangeColorSlice((storeFaceIndex + 2) % sliceCount, mat2);
+            groundSections.rings[0].sections[(storeFaceIndex + 2) % sliceCount].hurts = true;
+            groundSections.rings[1].sections[(storeFaceIndex + 2) % sliceCount].hurts = true;
+            groundSections.rings[2].sections[(storeFaceIndex + 2) % sliceCount].hurts = true;
 
 
-        if (SongManager.Instance.SongPositionInBeats >= 4 && SongManager.Instance.SongPositionInBeats < 5)
-        {
-            if (noteToPlayInSeconds - SongManager.Instance.SongPositionInSeconds <= 0)
-            {
+            ground.ChangeColorSlice((storeFaceIndex - 2) % sliceCount, mat2);
+            groundSections.rings[0].sections[(storeFaceIndex - 2) % sliceCount].hurts = true;
+            groundSections.rings[1].sections[(storeFaceIndex - 2) % sliceCount].hurts = true;
+            groundSections.rings[2].sections[(storeFaceIndex - 2) % sliceCount].hurts = true;
 
-                FaceIndex = player.faceIndex;
-                storeFaceIndex = player.faceIndex + SliceCount;
-                allredFaceIndex = player.faceIndex + SliceCount;
+            faceIndex += sliceCount;
+        }
+    }
 
-                Vector3 spawnPos = new Vector3(groundSections.rings[2].faces[FaceIndex].position.x, spawnHeight,
-                    groundSections.rings[2].faces[FaceIndex].position.z);
-                Vector3 endPos = new Vector3(groundSections.rings[2].faces[FaceIndex].position.x,
-                    endHeight, groundSections.rings[2].faces[FaceIndex].position.z);
+    public void AttackPhase2(float noteToPlayInSeconds){
 
-                float WallTime = Vector3.Distance(spawnPos, endPos) / WallSpeed;
+        if (noteToPlayInSeconds - SongManager.Instance.SongPositionInSeconds <= 0){
 
-                Wall wl;
-                wl = Instantiate(wall, spawnPos, Quaternion.identity);
-                wl.MoveUpDown(spawnPos, endPos, WallTime, WaitingTime);
-                ground.AllRed((storeFaceIndex + 2) % SliceCount);
-                ground.AllRed((storeFaceIndex - 2) % SliceCount);
-
-                //AttackPhase2(storeFaceIndex, SliceCount, noteToPlayInSeconds);
-
-                //for (int i = 0; i < 5; i++)
-                //{
-                //    ground.AllRed(0, allredFaceIndex +2 + i);
-                //    ground.AllRed(1, allredFaceIndex + 2 + i);
-                //    ground.AllRed(2, allredFaceIndex + 2 + i);
-                //}
-
-                //ground.ChangeColorSlice((FaceIndex + 1) % SliceCount);
-                //ground.ChangeColorSlice((FaceIndex -1) % SliceCount);
-
-                FaceIndex += SliceCount;
+            if (!set) {
+                faceIndex = (player.faceIndex + sliceCount);
+                direction = faceIndex - storeFaceIndex;
+                index = (faceIndex + direction) % sliceCount;                
+                set = true;
+            } else {
+                index = (index + sliceCount + direction) % sliceCount;
             }
+            //Debug.Log(index);
+            ground.ChangeColorSlice(index, mat1);
+            groundSections.rings[0].sections[index].hurts = false;
+            groundSections.rings[1].sections[index].hurts = false;
+            groundSections.rings[2].sections[index].hurts = false;
 
-        }
+            ground.ChangeColorSlice((index + sliceCount + 1) % sliceCount, mat2);
+            groundSections.rings[0].sections[(index + sliceCount + 1) % sliceCount].hurts = true;
+            groundSections.rings[1].sections[(index + sliceCount + 1) % sliceCount].hurts = true;
+            groundSections.rings[2].sections[(index + sliceCount + 1) % sliceCount].hurts = true;
 
-
-        AttackPhase2(storeFaceIndex, SliceCount, noteToPlayInSeconds);
-
-    }
-
-
-
-    public void AttackPhase2(int storeFaceIndex, int SliceCount, float noteToPlayInSeconds)
-    {
-
-
-        if (SongManager.Instance.SongPositionInBeats >= 4)// && SongManager.Instance.SongPositionInBeats < 16)
-        {
-            if (noteToPlayInSeconds - SongManager.Instance.SongPositionInSeconds <= 0)
-            {
-                FaceIndex = (player.faceIndex + SliceCount + y);
-                //storeFaceIndex = storeFaceIndex % SliceCount;
+            ground.ChangeColorSlice((index + sliceCount - 1) % sliceCount, mat2);
+            groundSections.rings[0].sections[(index + sliceCount - 1) % sliceCount].hurts = true;
+            groundSections.rings[1].sections[(index + sliceCount - 1) % sliceCount].hurts = true;
+            groundSections.rings[2].sections[(index + sliceCount - 1) % sliceCount].hurts = true;
 
 
-                Debug.Log("face" + (FaceIndex % SliceCount));
-                Debug.Log("store" + (storeFaceIndex % SliceCount));
+            //ground.AllBlue(index);
+            //ground.AllRed((index + sliceCount + 1) % sliceCount);
+            //ground.AllRed((index + sliceCount - 1) % sliceCount);
 
+            //Debug.Log("face" + (faceIndex % sliceCount));
+            //Debug.Log("store" + (storeFaceIndex % sliceCount));
 
-                if ((storeFaceIndex % SliceCount) == 8 && (FaceIndex % SliceCount) == 0)
-                {
-                    Debug.Log("caso speciale uno");
-                    dx = 1;
-                    SliceAttackRight(storeFaceIndex, SliceCount);
-                    y++;
-                }
+            //if ((storeFaceIndex % sliceCount) == 8 && (faceIndex % sliceCount) == 0){
+            //    Debug.Log("caso speciale uno");
+            //    dx = 1;
+            //    SliceAttackRight(storeFaceIndex, sliceCount);
+            //    y++;
+            //}
 
-                if ((storeFaceIndex % SliceCount) == 0 && (FaceIndex % SliceCount) == 8)
-                {
-                    Debug.Log("caso speciale due");
-                    sx = 1;
-                    SliceAttackLeft(storeFaceIndex, SliceCount);
-                    y++;
-                }
+            //if ((storeFaceIndex % sliceCount) == 0 && (faceIndex % sliceCount) == 8){
+            //    Debug.Log("caso speciale due");
+            //    sx = 1;
+            //    SliceAttackLeft(storeFaceIndex, sliceCount);
+            //    y++;
+            //}
 
-                else
-                {
-                    if (sx == 0 && (storeFaceIndex % SliceCount) < (FaceIndex % SliceCount))
-                    {
-                        Debug.Log("destra normale");
-                        dx = 1;
-                        SliceAttackRight(storeFaceIndex, SliceCount);
-                        storeFaceIndex++;
-                        FaceIndex++;
-                        y++;
-                    }
+            //else{
+            //    if (sx == 0 && (storeFaceIndex % sliceCount) < (faceIndex % sliceCount)){
+            //        Debug.Log("destra normale");
+            //        dx = 1;
+            //        SliceAttackRight(storeFaceIndex, sliceCount);
+            //        storeFaceIndex++;
+            //        faceIndex++;
+            //        y++;
+            //    }
 
-                    if (dx == 0 && (storeFaceIndex % SliceCount) > (FaceIndex % SliceCount))
-                    {
-                        Debug.Log("sinistra normale");
-                        sx = 1;
-                        SliceAttackLeft(storeFaceIndex, SliceCount);
-                        storeFaceIndex++;
-                        FaceIndex++;
-                        y++;
-                    }
-                }
-
-                //if (storeFaceIndex == FaceIndex)
-                //{
-                //    Debug.Log("HIT BY THE WALL");
-                //}
-                //else
-                //{
-
-                //    if (sx == 0 && storeFaceIndex < FaceIndex && i != 8)
-                //    {
-                //        Debug.Log("face again " + FaceIndex);
-
-                //        dx = 1;
-
-                //        newFaceIndex = i + storeFaceIndex;
-
-                //        Debug.Log("new " + newFaceIndex);
-                //        Debug.Log("new + 2 " + ((newFaceIndex + 2) % SliceCount));
-                //        Debug.Log(" i" + i);
-
-                //        ground.ChangeColorSlice((newFaceIndex + 2) % SliceCount);
-                //        ground.AllBlue((newFaceIndex + 1) % SliceCount);
-                //        ground.AllRed((newFaceIndex) % SliceCount);
-                //        i++;
-                //    }
-
-                //    if (dx == 0 && storeFaceIndex > (FaceIndex % SliceCount) && i != 8)
-                //    {
-                //        Debug.Log("face again " + FaceIndex);
-
-                //        sx = 1;
-
-                //        newFaceIndex = storeFaceIndex - i;
-
-                //        ground.ChangeColorSlice((newFaceIndex - 2) % SliceCount);
-                //        ground.AllBlue((newFaceIndex - 1) % SliceCount);
-                //        ground.AllRed((newFaceIndex) % SliceCount);
-                //        i++;
-                //    }
-
-                //}
-
-
-
-            }
-
-
+            //    if (dx == 0 && (storeFaceIndex % sliceCount) > (faceIndex % sliceCount)){
+            //        Debug.Log("sinistra normale");
+            //        sx = 1;
+            //        SliceAttackLeft(storeFaceIndex, sliceCount);
+            //        storeFaceIndex++;
+            //        faceIndex++;
+            //        y++;
+            //    }
+            //}
         }
     }
 
-    public void SliceAttackRight(int storeFaceIndex, int SliceCount)
-    {
-        if (i != 8)
-        {
-            Debug.Log("i " + i);
-            newFaceIndex = i + storeFaceIndex;
+    //public void SliceAttackRight(int storeFaceIndex, int SliceCount)
+    //{
+    //    if (i != 8)
+    //    {
+    //        Debug.Log("i " + i);
+    //        newFaceIndex = i + storeFaceIndex;
 
-            ground.ChangeColorSlice((newFaceIndex + 3) % SliceCount);
-            ground.AllBlue((newFaceIndex + 2) % SliceCount);
-            ground.AllRed((newFaceIndex + 1) % SliceCount);
-            ground.AllRed((newFaceIndex) % SliceCount);
-            i++;
-        }
-    }
+    //        ground.ChangeColorSlice((newFaceIndex + 3) % SliceCount);
+    //        ground.AllBlue((newFaceIndex + 2) % SliceCount);
+    //        ground.AllRed((newFaceIndex + 1) % SliceCount);
+    //        ground.AllRed((newFaceIndex) % SliceCount);
+    //        i++;
+    //    }
+    //}
 
-    public void SliceAttackLeft(int storeFaceIndex, int SliceCount)
-    {
-        if (i != 8)
-        {
-            Debug.Log("i " + i);
-            newFaceIndex = storeFaceIndex - i;
+    //public void SliceAttackLeft(int storeFaceIndex, int SliceCount)
+    //{
+    //    if (i != 8)
+    //    {
+    //        Debug.Log("i " + i);
+    //        newFaceIndex = storeFaceIndex - i;
 
-            ground.ChangeColorSlice((newFaceIndex - 3) % SliceCount);
-            ground.AllBlue((newFaceIndex - 2) % SliceCount);
-            ground.AllRed((newFaceIndex - 1) % SliceCount);
-            ground.AllRed((newFaceIndex) % SliceCount);
-            i++;
-        }
-    }
+    //        ground.ChangeColorSlice((newFaceIndex - 3) % SliceCount);
+    //        ground.AllBlue((newFaceIndex - 2) % SliceCount);
+    //        ground.AllRed((newFaceIndex - 1) % SliceCount);
+    //        ground.AllRed((newFaceIndex) % SliceCount);
+    //        i++;
+    //    }
+    //}
 
     //public void SliceAttackLeft(int storeFaceIndex, int SliceCount)
     //{
@@ -241,6 +195,19 @@ public class SliceAttack : MonoBehaviour
     //    }
     //}
 
+    public void ClearSections() {
+
+        for (int i = 0; i < groundSections.rings.Count; i++) {
+            for (int j = 0; j < groundSections.rings[i].sections.Count; j++) {
+                groundSections.rings[i].sections[j].hurts = false;
+                ground.ChangeColor(i, j, mat1);
+            }
+        }
+
+        Destroy(wall.gameObject);
+        set = false;
+        //firstAtt = true;
+    }
 
 }
 
