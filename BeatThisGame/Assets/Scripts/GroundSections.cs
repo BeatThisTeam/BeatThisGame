3acff93652867a0817ca425d6e2abee2fdd546c7
Giacomo <jack.vannu@gmail.com>
2018-12-07 09:32:48 +0100

    Added functions to switch tiles in hurt state and damage player

--- BeatThisGame/Assets/Scripts/GroundSections.cs
+++ BeatThisGame/Assets/Scripts/GroundSections.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
-using ProBuilder.Addons;
-using ProBuilder.Core;
-using ProBuilder.MeshOperations;
 
 public class GroundSections : MonoBehaviour {
 
@@ -15,4 +12,43 @@ public class GroundSections : MonoBehaviour {
     }
 
     public List<Ring> rings = new List<Ring>();
+
+    IEnumerator SetHurt(int ringIndex, int faceIndex, float delay) {
+
+        yield return new WaitForSeconds(delay);
+        rings[ringIndex].sections[faceIndex].hurts = true;
+        Hurts(ringIndex, faceIndex);
+    }
+
+    public void ResetHurt(int ringIndex, int faceIndex) {
+
+        rings[ringIndex].sections[faceIndex].hurts = false;
+    }
+
+    public void SwitchFace(int ringIndex, int faceIndex) {
+
+        if(rings[ringIndex].sections[faceIndex].hurts == true) {
+            ResetHurt(ringIndex, faceIndex);
+        } else {
+            StartCoroutine(SetHurt(ringIndex, faceIndex, 0.1f));
+        }
+    }
+
+    public void SwitchFace(int ringIndex, int faceIndex, bool val) {
+
+        if (val == false) {
+            ResetHurt(ringIndex, faceIndex);
+        } else {
+            StartCoroutine(SetHurt(ringIndex, faceIndex, 0.1f));
+        }
+    }
+
+    public void Hurts(int ringIndex, int faceIndex) {
+
+        CharacterController player = ScenePrototypeManager.Instance.player.GetComponent<CharacterController>();
+
+        if(rings[ringIndex].sections[faceIndex].hurts && player.ringIndex == ringIndex && player.faceIndex == faceIndex) {
+            player.Damage(10);
+        }
+    }
 }
