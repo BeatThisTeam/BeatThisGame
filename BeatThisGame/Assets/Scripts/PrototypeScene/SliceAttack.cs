838d965995e4eadb4efe77fa8556c621232fe5fa
Giacomo <jack.vannu@gmail.com>
2018-12-08 13:22:34 +0100

    Used CharacterController.Direction to choose the direction of the attack

--- BeatThisGame/Assets/Scripts/PrototypeScene/SliceAttack.cs
+++ BeatThisGame/Assets/Scripts/PrototypeScene/SliceAttack.cs
@@ -7,17 +7,10 @@ public class SliceAttack : MonoBehaviour
     public Transform _wall;
 
     public int LimitTime;
-
-    int RingIndex;
+    
     int faceIndex;
-    int newFaceIndex = 0;
     int storeFaceIndex;
     int allredFaceIndex;
-
-    int i = 0;
-    int sx = 0;
-    int dx = 0;
-    int y = 0;
     private bool set = false;
 
     //-1 left +1 right
@@ -27,8 +20,6 @@ public class SliceAttack : MonoBehaviour
     public float endHeight = 7f;
     public float spawnHeight = 7f;
     int sliceCount;
-    public Wall wallPrefab;
-    private Wall wall;
 
     public BossController bossContr;
 
@@ -43,65 +34,71 @@ public class SliceAttack : MonoBehaviour
 
         sliceCount = groundSections.rings[0].sections.Count;
 
-        if (noteToPlayInSeconds - SongManager.Instance.SongPositionInSeconds <= 0){
+        faceIndex = player.faceIndex;
+        storeFaceIndex = player.faceIndex + sliceCount;
+        Debug.Log("sfi" + storeFaceIndex);
+        allredFaceIndex = player.faceIndex + sliceCount;
 
-            faceIndex = player.faceIndex;
-            storeFaceIndex = player.faceIndex + sliceCount;
-            allredFaceIndex = player.faceIndex + sliceCount;
+        Vector3 spawnPos = new Vector3(groundSections.rings[2].sections[faceIndex].tr.position.x, spawnHeight, groundSections.rings[2].sections[faceIndex].tr.position.z);
+        Vector3 endPos = new Vector3(groundSections.rings[2].sections[faceIndex].tr.position.x, endHeight, groundSections.rings[2].sections[faceIndex].tr.position.z);
 
-            Vector3 spawnPos = new Vector3(groundSections.rings[2].sections[faceIndex].tr.position.x, spawnHeight, groundSections.rings[2].sections[faceIndex].tr.position.z);
-            Vector3 endPos = new Vector3(groundSections.rings[2].sections[faceIndex].tr.position.x, endHeight, groundSections.rings[2].sections[faceIndex].tr.position.z);
+        float WallTime = ScenePrototypeManager.Instance.notesInSeconds[ScenePrototypeManager.Instance.notesInSecondsIndex + 1].notePosInSeconds - noteToPlayInSeconds;
 
-            float WallTime = ScenePrototypeManager.Instance.notesInSeconds[ScenePrototypeManager.Instance.notesInSecondsIndex + 1].notePosInSeconds - noteToPlayInSeconds;
+        Vector3 lookAtPos = _player.position - bossContr.transform.position;
+        lookAtPos.y = 0;
+        bossContr.transform.rotation = Quaternion.LookRotation(lookAtPos);
+        bossContr.transform.Rotate(0, 90, 0);
+        bossContr.StartSlam(WallTime);
 
-            Vector3 lookAtPos = _player.position - bossContr.transform.position;
-            lookAtPos.y = 0;
-            bossContr.transform.rotation = Quaternion.LookRotation(lookAtPos);
-            bossContr.transform.Rotate(0, 90, 0);
-            bossContr.StartSlam(WallTime);
+        ground.ChangeColorSlice((storeFaceIndex + 2) % sliceCount, mat2);
+        groundSections.SwitchFace(0, (storeFaceIndex + 2) % sliceCount, true);
+        groundSections.SwitchFace(1, (storeFaceIndex + 2) % sliceCount, true);
+        groundSections.SwitchFace(2, (storeFaceIndex + 2) % sliceCount, true);
 
-            ground.ChangeColorSlice((storeFaceIndex + 2) % sliceCount, mat2);
-            groundSections.SwitchFace(0, (storeFaceIndex + 2) % sliceCount, true);
-            groundSections.SwitchFace(1, (storeFaceIndex + 2) % sliceCount, true);
-            groundSections.SwitchFace(2, (storeFaceIndex + 2) % sliceCount, true);
+        ground.ChangeColorSlice((storeFaceIndex - 2) % sliceCount, mat2);
+        groundSections.SwitchFace(0, (storeFaceIndex - 2) % sliceCount, true);
+        groundSections.SwitchFace(1, (storeFaceIndex - 2) % sliceCount, true);
+        groundSections.SwitchFace(2, (storeFaceIndex - 2) % sliceCount, true);
 
-            ground.ChangeColorSlice((storeFaceIndex - 2) % sliceCount, mat2);
-            groundSections.SwitchFace(0, (storeFaceIndex - 2) % sliceCount, true);
-            groundSections.SwitchFace(1, (storeFaceIndex - 2) % sliceCount, true);
-            groundSections.SwitchFace(2, (storeFaceIndex - 2) % sliceCount, true);
-
-            faceIndex += sliceCount;
-        }
+        faceIndex += sliceCount;
     }
 
     public void AttackPhase2(float noteToPlayInSeconds){
 
-        if (noteToPlayInSeconds - SongManager.Instance.SongPositionInSeconds <= 0){
+        if (!set) {
+            faceIndex = (player.faceIndex + sliceCount);
 
-            if (!set) {
-                faceIndex = (player.faceIndex + sliceCount);
-                direction = faceIndex - storeFaceIndex;
-                index = (faceIndex + direction) % sliceCount;                
-                set = true;
+            if(player.Dir == CharacterController.Direction.Left) {
+                direction = -1;
+            }else if(player.Dir == CharacterController.Direction.Right) {
+                direction = 1;
+            } else if(Random.value > 0.5){
+                direction = -1;
             } else {
-                index = (index + sliceCount + direction) % sliceCount;
+                direction = 1;
             }
 
-            ground.ChangeColorSlice(index, mat1);
-            groundSections.SwitchFace(0, index, false);
-            groundSections.SwitchFace(1, index, false);
-            groundSections.SwitchFace(2, index, false);
-
-            ground.ChangeColorSlice((index + sliceCount + 1) % sliceCount, mat2);
-            groundSections.SwitchFace(0, (index + sliceCount + 1) % sliceCount, true);
-            groundSections.SwitchFace(1, (index + sliceCount + 1) % sliceCount, true);
-            groundSections.SwitchFace(2, (index + sliceCount + 1) % sliceCount, true);
-
-            ground.ChangeColorSlice((index + sliceCount - 1) % sliceCount, mat2);
-            groundSections.SwitchFace(0, (index + sliceCount - 1) % sliceCount, true);
-            groundSections.SwitchFace(1, (index + sliceCount - 1) % sliceCount, true);
-            groundSections.SwitchFace(2, (index + sliceCount - 1) % sliceCount, true);
+            Debug.Log("dir" + direction);
+            index = (faceIndex + direction) % sliceCount;                
+            set = true;
+        } else {
+            index = (index + sliceCount + direction) % sliceCount;
         }
+        Debug.Log(index);
+        ground.ChangeColorSlice(index, mat1);
+        groundSections.SwitchFace(0, index, false);
+        groundSections.SwitchFace(1, index, false);
+        groundSections.SwitchFace(2, index, false);
+
+        ground.ChangeColorSlice((index + sliceCount + 1) % sliceCount, mat2);
+        groundSections.SwitchFace(0, (index + sliceCount + 1) % sliceCount, true);
+        groundSections.SwitchFace(1, (index + sliceCount + 1) % sliceCount, true);
+        groundSections.SwitchFace(2, (index + sliceCount + 1) % sliceCount, true);
+
+        ground.ChangeColorSlice((index + sliceCount - 1) % sliceCount, mat2);
+        groundSections.SwitchFace(0, (index + sliceCount - 1) % sliceCount, true);
+        groundSections.SwitchFace(1, (index + sliceCount - 1) % sliceCount, true);
+        groundSections.SwitchFace(2, (index + sliceCount - 1) % sliceCount, true);
     }
 
     public void Return() {
