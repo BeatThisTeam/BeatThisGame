a8802d5e3892f122e8b086ef4a75c3212979f42b
alepetrali <alejandra.imaz@mail.polimi.it>
2018-12-10 17:11:28 +0100

    UI + scripts added to the scene

---
+++ BeatThisGame/Assets/Scripts/BossHealth.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BossHealth : MonoBehaviour {
+
+    int minValue = 0, maxValue = 100, variationAmount = 2;
+    Slider sliderRef;                                               // True when the boss gets damaged.
+
+    void Start () {
+        sliderRef = this.gameObject.GetComponent<Slider>();
+        sliderRef.minValue = minValue;
+        sliderRef.maxValue = maxValue;
+    }
+	
+	void Update () {
+        if (Input.GetKeyDown("a"))
+        {
+
+            sliderRef.value -= variationAmount;
+        }
+        else if (Input.GetKeyDown("d"))
+        {
+            sliderRef.value += variationAmount;
+        }
+    }
+}
---
+++ BeatThisGame/Assets/Scripts/PowerAttack.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerAttack : MonoBehaviour 
+{
+    int minValue = 0, maxValue = 100, variationAmount = 2;
+    Slider sliderRef;                                               // True when the player gets damaged.
+
+
+    void Start()
+    {
+        sliderRef = this.gameObject.GetComponent<Slider>();
+        sliderRef.minValue = minValue;
+        sliderRef.maxValue = maxValue;
+        sliderRef.value = minValue;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown("a"))
+        {
+
+            sliderRef.value -= variationAmount;
+        }
+        else if (Input.GetKeyDown("d"))
+        {
+            sliderRef.value += variationAmount;
+        }
+    }
+
+}
