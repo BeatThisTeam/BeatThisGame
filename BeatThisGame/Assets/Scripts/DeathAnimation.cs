365992c5efd354c10afb2c3f5146dac76aba1f5c
anna <>
2019-01-03 10:58:10 +0100

    animations updated, Teleport and Shield materials added

---
+++ BeatThisGame/Assets/Scripts/DeathAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathAnimation : MonoBehaviour {
+
+    private Animator anim;
+    private Material[] materials;
+    private float i;
+
+    public GameObject LOD;
+    public float transitionSpeed;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+
+        materials = LOD.GetComponent<Renderer>().materials;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(anim.GetBool("isDead") && i < 1.5)
+        {
+            //Debug.Log("C is dead");
+            foreach (Material material in materials)
+            {
+                i = Mathf.Lerp(i, 1, Time.deltaTime * transitionSpeed);
+                material.SetFloat("Vector1_D4B3BD5A", i);
+                material.SetFloat("Vector1_DC68DE65", i);
+                Debug.Log("i is " + i);
+            }
+        }
+	}
+}
