5ac4e9867ab55a2338a1128ff8076e1ddbbb8015
Giacomo <jack.vannu@gmail.com>
2019-01-07 14:48:31 +0100

    Merge branch 'master' of https://github.com/BeatThisTeam/BeatThisGame
    # Conflicts:
    #	BeatThisGame/Assets/Scenes/Stages/Stage1.unity
    #	BeatThisGame/Assets/Scenes/Stages/Stage2.unity

---
+++ BeatThisGame/Assets/Scripts/Tornado.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tornado : MonoBehaviour {
+
+    public float degrees = 20;
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.RotateAround(gameObject.transform.position, Vector3.forward, degrees * Time.deltaTime);
+    }
+}
