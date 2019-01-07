b2d52d7d0a3529e97ca60234dd7d6b84fe447292
anna <>
2019-01-07 14:14:48 +0100

    fixed Blob idle, added assets for second stage

---
+++ BeatThisGame/Assets/Scripts/SelfRotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfRotation : MonoBehaviour {
+
+    public float speed;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, Time.deltaTime * speed, 0));
+    }
+}
